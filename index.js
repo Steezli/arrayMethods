90ca185405d66bd78fe0bd77e14f0563299924be
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-25 11:06:55 -0700

    working tests for indexOf

--- index.js
+++ index.js
@@ -11,15 +11,25 @@ function filter(array, callback) {
   const filteredArray = [];
   let i = 0;
   for(i; i < array.length; i++) {
-    if(callback(Array[i])) {
+    if(callback(array[i])) {
       filteredArray[filteredArray.length] = array[i];
     }
   }
   return filteredArray;
 }
 
+function indexOf(array, callback) {
+  for(let i = 0; i < array.length; i++) {
+    if(callback(array[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 const numsArray = [1, 2, 4, 6, 10];
 console.log(map(numsArray, number => number * number));
 console.log(filter(numsArray, num => num === 2));
+// console.log(indexOf());
 
-module.exports = { map, filter };
+module.exports = { map, filter, indexOf }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 
-const { map, filter } = require('./index');
+const { map, filter, indexOf } = require('./index');
 
 describe('array methods', () => {
   it('map and return newArray ', () => {
@@ -8,21 +8,35 @@ describe('array methods', () => {
     expect(sqaured).toEqual([1, 4, 16, 36, 100]);
   });
 
-  it('filter and return an array ', () => {
-    const numsArray = [1, 2, 4, 6, 10];
-    const filtered = filter(numsArray, num => num.length < 6);
-    expect(filtered).toEqual(expect.any(Array));
+  // it('filter and return an array ', () => {
+  //   const numsArray = [1, 2, 4, 6, 10];
+  //   const filtered = filter(numsArray, num => num.length < 6);
+  //   expect(filtered).toEqual(expect.any(Array));
+  // });
+
+  // it('filter and return newArray ', () => {
+  //   const numsArray = [1, 2, 3, 4];
+  //   const evens = filter(numsArray, num => num % 2 === 0);
+  //   expect(evens).toEqual([2, 4]);
+  // });
+
+  // it('filter odd numbers out ', () => {
+  //   const numsArray = [1, 2, 3, 4];
+  //   const odds = filter(numsArray, num => num % 3 === 0);
+  //   expect(odds).toEqual([1, 3]);
+  // });
+});
+
+describe('indexOf method', () => {
+  it('find index', () => {
+    const numsArray = [1, 2, 3, 4];
+    const index = indexOf(numsArray, num => num > 2);
+    expect(index).toEqual(2);
   });
 
-  it('filter and return newArray ', () => {
+  it('find no index', () => {
     const numsArray = [1, 2, 3, 4];
-    const evens = filter(numsArray, num => num % 2 === 0);
-    expect(evens).toEqual([2, 4]);
-  });
-
-  it('filter odd numbers out ', () => {
-    const numsArray = [1, 2, 3, 4];
-    const odds = filter(numsArray, num => num % 3 === 0);
-    expect(odds).toEqual([1, 3]);
+    const index = indexOf(numsArray, num => num > 5);
+    expect(index).toEqual(-1);
   });
 });
