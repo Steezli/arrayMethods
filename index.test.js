8408aae02d4a83af77825f93c4c148e78acd6613
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-25 09:10:37 -0700

    working map array test

---
+++ index.test.js
@@ -0,0 +1,10 @@
+
+const { map } = require('./index');
+
+describe('array method test', () => {
+  it('map and return newArray ', () => {
+    const array = [4, 6, 10];
+    const sqaured = map(array, number => number * number);
+    expect(sqaured).toEqual([16, 36, 100]);
+  });
+}); 
